8b4e600a95382f0482d2e0cd83c0700ba0e3f46b
Gleb Skotnikov <skotnikov.nsk@gmail.com>
2023-06-20 22:59:10 +0600

    Add code and tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,8 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+def test_class_item(test_example):
+    assert test_example.name == "Смартфон"
+    assert test_example.price == 10000
+    assert test_example.quantity == 20
+    assert test_example.calculate_total_price() == 200000
+    test_example.apply_discount()
+    assert test_example.price == 8000
