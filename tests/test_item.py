5b3ffc1acd8be6ac960c686a80493ba4bd28083a
Gleb Skotnikov <skotnikov.nsk@gmail.com>
2023-06-24 21:15:35 +0700

    Add code and tests (homework_2)

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,8 +1,30 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-def test_class_item(test_example):
+from src.item import Item
+
+
+def test_class_item_init(test_example):
     assert test_example.name == "Смартфон"
     assert test_example.price == 10000
     assert test_example.quantity == 20
+
+
+def test_calculate_total_price(test_example):
     assert test_example.calculate_total_price() == 200000
+
+
+def test_apply_discount(test_example):
     test_example.apply_discount()
     assert test_example.price == 8000
+
+
+def test_name_setter(test_example):
+    test_example.name = "Телефон"
+    assert test_example.name == "Телефон"
+    test_example.name = "Супер Телефон"
+    assert test_example.name == "Супер Теле"
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
